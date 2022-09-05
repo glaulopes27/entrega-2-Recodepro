1b7ebbe6a2d61fe61823d41e39ca494d12300c3a
Glaucia <glauciagolopes@gmail.com>
2022-09-05 18:47:46 -0300

    entrega2

---
+++ arquivomysql.sql
@@ -0,0 +1,122 @@
+CREATE DATABASE  IF NOT EXISTS `agencia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `agencia`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: agencia
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `agencia`
+--
+
+DROP TABLE IF EXISTS `agencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agencia` (
+  `id_func` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) DEFAULT NULL,
+  `cpf` char(11) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `telefone` char(13) DEFAULT NULL,
+  `senha` char(6) DEFAULT NULL,
+  PRIMARY KEY (`id_func`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agencia`
+--
+
+LOCK TABLES `agencia` WRITE;
+/*!40000 ALTER TABLE `agencia` DISABLE KEYS */;
+INSERT INTO `agencia` VALUES (1,'Mario','12866678733','email@viagem.com.br','1144223001','1234'),(2,'Maria','55566678743','email@viagem.com.br','1144223002','1235'),(3,'Karina','54867778744','email@viagem.com.br','1144223003','1236'),(4,'julia','89755678766','email@viagem.com.br','1144223004','1237'),(5,'Marcela','99866978788','email@viagem.com.br','1144223005','1238');
+/*!40000 ALTER TABLE `agencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `nome_destino` varchar(20) NOT NULL,
+  `id_destino` int DEFAULT NULL,
+  `data_viagem` date DEFAULT NULL,
+  `datas_disponiveis` date DEFAULT NULL,
+  PRIMARY KEY (`id_cliente`),
+  KEY `id_destino_idx` (`id_destino`),
+  CONSTRAINT `id_destino` FOREIGN KEY (`id_destino`) REFERENCES `destinos` (`id_destino`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Paulo','22266677766','email@gmail.com','Salvador',NULL,'2023-01-02',NULL),(2,'Marcelo','22866677766','email@gmail.com','Recife',NULL,'2023-02-23',NULL),(3,'Anna','24266677766','email@gmail.com','Alagoas',NULL,'2022-02-23',NULL),(4,'Joana','12266677766','email@gmail.com','Porto de Galinhas',NULL,'2023-01-07',NULL),(5,'Carlos','22966677766','email@gmail.com','Rio de Janeiro',NULL,'2023-05-11',NULL);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `destinos`
+--
+
+DROP TABLE IF EXISTS `destinos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `destinos` (
+  `id_destino` int NOT NULL AUTO_INCREMENT,
+  `nome_destino` varchar(20) DEFAULT NULL,
+  `valor_pacote` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`id_destino`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destinos`
+--
+
+LOCK TABLES `destinos` WRITE;
+/*!40000 ALTER TABLE `destinos` DISABLE KEYS */;
+INSERT INTO `destinos` VALUES (1,'copacabana',1599.00),(2,'rio grande do norte',2499.00),(3,'costa do sauipe',1599.00),(4,'maceio',2499.00),(5,'morogoci',2499.00);
+/*!40000 ALTER TABLE `destinos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'agencia'
+--
+
+--
+-- Dumping routines for database 'agencia'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-05 17:59:53
